07998fd30c347cd77b4f5a5b7eb77c8a4b5ec52e
jake <palatable7@naver.com>
2022-05-07 12:50:12 +0900

    Add

--- ExLocalNoti/AppDelegate.swift
+++ ExLocalNoti/AppDelegate.swift
@@ -39,12 +39,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
   }
-
-
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
   func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
     [.list, .banner, .sound, .badge]
+  }
+  func userNotificationCenter(
+    _ center: UNUserNotificationCenter,
+    didReceive response: UNNotificationResponse,
+    withCompletionHandler completionHandler: @escaping () -> Void
+  ) {
+    if response.notification.request.identifier == "local noti" {
+      print("딥링크 수행")
+    }
+    completionHandler()
   }
 }
